5a7b30df06e9d6449ed0547f568369213f5c494a
Zhao vistart <i@vistart.vip>
2017-03-24 21:10:31 +0800

    update messages.

---
+++ messages/zh-CN/user.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ *  _   __ __ _____ _____ ___  ____  _____
+ * | | / // // ___//_  _//   ||  __||_   _|
+ * | |/ // /(__  )  / / / /| || |     | |
+ * |___//_//____/  /_/ /_/ |_||_|     |_|
+ * @link https://vistart.me/
+ * @copyright Copyright (c) 2016 - 2017 vistart
+ * @license https://vistart.me/license/
+ */
+
+return [
+    'Password' => '密码',
+    'New Password' => '新密码',
+    'New Password Repeat' => '重复新密码',
+    'Change Password' => '更改密码',
+    'Security' => '安全',
+    'GUID' => '全球唯一编码',
+    'Password Hash' => '密码哈希',
+    'IP Address' => 'IP 地址',
+    'IP Address Type' => 'IP 地址类型',
+    'Creation Time' => '创建时间',
+    'Last Updated Time' => '上次更新时间',
+    'Authentication Key' => '验证密钥',
+    'Access Token' => '访问令牌',
+    'Password Reset Token' => '密码重置令牌',
+    'Status' => '状态',
+    'Type' => '类型',
+    'Source' => '来源',
+    'ID' => '账号',
+    'Remember Me' => '记住我',
+    'Login' => '登录',
+    'Register' => '注册',
+    'Nickname' => '昵称',
+    'Password Repeat' => '重复密码',
+    'First Name' => '名',
+    'Last Name' => '姓',
+    'Gender' => '性别',
+    'Gravatar Type' => '头像类型',
+    'Gravatar' => '头像',
+    'Timezone' => '时区',
+    'Register Failed' => '注册失败',
+    'Admin' => '管理员',
+    'User Not Found.' => '用户未找到。',
+    'You cannot deregister yourself.' => '您不能删除自己。',
+    'Failed to deregister user.' => '未能注销用户。',
+    'Action' => '动作',
+    'You are not an administrator and have no access to this page.' => '您不是管理员，无权访问此页面。',
+    'You cannot access this page directly.' => '您不能直接访问此页面。',
+    'You must log out current user before registering new one.' => '在注册新用户前，你必须登出当前用户。',
+    'Password Not Changed.' => '密码未修改。',
+    'Password Changed.' => '密码已修改',
+    'User List' => '用户列表',
+];
