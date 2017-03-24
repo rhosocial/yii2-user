2711031206e7ae59592ae467d68e4de2fe1d99cb
Zhao vistart <i@vistart.vip>
2017-03-24 21:18:50 +0800

    update test case.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -69,6 +69,18 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase {
                     'basePath' => __DIR__,
                     'vendorPath' => dirname(__DIR__) . '/vendor',
                     'components' => [
+                        'i18n' => [
+                            'translations' => [
+                                'user*' => [
+                                    'class' => 'yii\i18n\PhpMessageSource',
+                                    'basePath' => dirname(__DIR__) . '/messages',
+                                    'sourceLanguage' => 'en-US',
+                                    'fileMap' => [
+                                        'user' => 'user.php',
+                                    ],
+                                ],
+                            ],
+                        ],
                         'request' => [
                             'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
                             'scriptFile' => __DIR__ . '/index.php',
